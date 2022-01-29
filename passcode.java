a9f122f0a8d0e7c9c18a86aeac8551f2c6788a52
Harsh Raj <harshraj6731@gmail.com>
2022-01-29 21:38:43 +0530

    initial commit

---
+++ passcode.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class passcode {
+    public static void main(String[] args) {
+        System.out.println("Enter number of digits:.");
+        Scanner sc = new Scanner(System.in);
+        int dig = sc.nextInt();
+        Random rd = new Random();
+        for (int i = 0; i < dig; i++) {
+            System.out.print(rd.nextInt(9));
+        }
+        sc.close();
+    }
+}
